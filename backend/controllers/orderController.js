38c30ad4fc380b2cffb75cf03586ad7acf162d68
Kartik Palan <kartikpalan1022@gmail.com>
2026-07-30 04:56:54 +0530

    Added the frontend url

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Placing user order for Frontend
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5174";
+    const frontend_url = "https://food-del-sigma-nine.vercel.app";
     try{
         const newOrder = new orderModel({
             userId: req.body.userId,
