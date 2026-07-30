100b8f21ae00d474d0875cc3d8120ab968c3c61f
Kartik Palan <kartikpalan1022@gmail.com>
2026-07-30 05:46:15 +0530

    Minute change in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import express from "express"
 import cors from "cors"
-import { connectDB } from "./config/db.js"
+import connectDB  from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import cartRouter from "./routes/cartRoute.js"
